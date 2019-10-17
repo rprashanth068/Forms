c8adb2906ccaf9fd14b825530afa5ab7f6e36fb3
rprashanth068 <51098238+rprashanth068@users.noreply.github.com>
2019-10-16 23:18:18 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import './App.css'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container myform">
+        <div className="row myform">
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>          
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>          
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>          
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>         
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>         
+          </div>
+          <div className="col-2">
+          <div class="form-group">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck" />
+            <label class="form-check-label" for="gridCheck">
+              Deceased
+            </label>
+          </div>
+        </div>
+        </div>          
+        </div>
+
+        <div className="row ">
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>          
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>          
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>          
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>
+          </div>         
+          <div className="col-2">          
+          <div class="form-group">
+            <label for="formGroupExampleInput">Example label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input" />
+          </div>         
+          </div>
+          <div className="col-2">
+          <div class="form-group">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck" />
+            <label class="form-check-label" for="gridCheck">
+              Deceased
+            </label>
+          </div>
+        </div>
+        </div>          
+        </div>
+        
+      </div>
+    )
+
+  }
+
+}
